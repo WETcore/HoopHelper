7e0e233c91121e05e06da7c63857e21d18232a93
Aqua <huangaqua@icloud.com>
2021-10-31 17:24:18 +0800

    add detail data on leaderboard the other cards

--- app/src/main/java/com/aqua/hoophelper/home/HomeFragment.kt
+++ app/src/main/java/com/aqua/hoophelper/home/HomeFragment.kt
@@ -52,6 +52,34 @@ class HomeFragment : Fragment() {
                 binding.scoreLeaderDetail.visibility = View.GONE
             }
         }
+        binding.reboundLeaderCard.setOnClickListener {
+            if (binding.reboundLeaderDetail.visibility == View.GONE) {
+                binding.reboundLeaderDetail.visibility = View.VISIBLE
+            } else {
+                binding.reboundLeaderDetail.visibility = View.GONE
+            }
+        }
+        binding.assistLeaderCard.setOnClickListener {
+            if (binding.assistLeaderDetail.visibility == View.GONE) {
+                binding.assistLeaderDetail.visibility = View.VISIBLE
+            } else {
+                binding.assistLeaderDetail.visibility = View.GONE
+            }
+        }
+        binding.stealLeaderCard.setOnClickListener {
+            if (binding.stealLeaderDetail.visibility == View.GONE) {
+                binding.stealLeaderDetail.visibility = View.VISIBLE
+            } else {
+                binding.stealLeaderDetail.visibility = View.GONE
+            }
+        }
+        binding.blockLeaderCard.setOnClickListener {
+            if (binding.blockLeaderDetail.visibility == View.GONE) {
+                binding.blockLeaderDetail.visibility = View.VISIBLE
+            } else {
+                binding.blockLeaderDetail.visibility = View.GONE
+            }
+        }
 
         viewModel.teamStat.observe(viewLifecycleOwner) {
             binding.scoreLeaderNum.text = viewModel.getLeaderboardData(DataType.SCORE)
@@ -80,6 +108,90 @@ class HomeFragment : Fragment() {
                 viewModel.getLeaderDetailData(DataType.SCORE, DetailDataType.BLK)
             binding.scoreDetailPf.text =
                 viewModel.getLeaderDetailData(DataType.SCORE, DetailDataType.PF)
+
+            binding.reboundDetailPts.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.PTS)
+            binding.reboundDetailFg.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.FG)
+            binding.reboundDetail3p.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.ThreeP)
+            binding.reboundDetailFt.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.FT)
+            binding.reboundDetailTov.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.TOV)
+            binding.reboundDetailReb.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.REB)
+            binding.reboundDetailAst.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.AST)
+            binding.reboundDetailStl.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.STL)
+            binding.reboundDetailBlk.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.BLK)
+            binding.reboundDetailPf.text =
+                viewModel.getLeaderDetailData(DataType.REBOUND, DetailDataType.PF)
+
+            binding.assistDetailPts.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.PTS)
+            binding.assistDetailFg.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.FG)
+            binding.assistDetail3p.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.ThreeP)
+            binding.assistDetailFt.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.FT)
+            binding.assistDetailTov.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.TOV)
+            binding.assistDetailReb.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.REB)
+            binding.assistDetailAst.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.AST)
+            binding.assistDetailStl.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.STL)
+            binding.assistDetailBlk.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.BLK)
+            binding.assistDetailPf.text =
+                viewModel.getLeaderDetailData(DataType.ASSIST, DetailDataType.PF)
+
+            binding.stealDetailPts.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.PTS)
+            binding.stealDetailFg.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.FG)
+            binding.stealDetail3p.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.ThreeP)
+            binding.stealDetailFt.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.FT)
+            binding.stealDetailTov.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.TOV)
+            binding.stealDetailReb.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.REB)
+            binding.stealDetailAst.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.AST)
+            binding.stealDetailStl.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.STL)
+            binding.stealDetailBlk.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.BLK)
+            binding.stealDetailPf.text =
+                viewModel.getLeaderDetailData(DataType.STEAL, DetailDataType.PF)
+
+            binding.blockDetailPts.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.PTS)
+            binding.blockDetailFg.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.FG)
+            binding.blockDetail3p.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.ThreeP)
+            binding.blockDetailFt.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.FT)
+            binding.blockDetailTov.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.TOV)
+            binding.blockDetailReb.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.REB)
+            binding.blockDetailAst.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.AST)
+            binding.blockDetailStl.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.STL)
+            binding.blockDetailBlk.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.BLK)
+            binding.blockDetailPf.text =
+                viewModel.getLeaderDetailData(DataType.BLOCK, DetailDataType.PF)
         }
 
         return binding.root
